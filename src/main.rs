e093c928751f86d1ac68f5b319139e2c09a5749b
Araozu <faraoz@unsa.edu.pe>
2021-02-09 10:35:25 -0500

    Agregar solucion dia 1 parte 2

--- src/main.rs
+++ src/main.rs
@@ -20,15 +20,25 @@ fn get_fuel(mass: i32) -> i32 {
     (mass / 3) - 2
 }
 
+fn get_recursive_fuel(mass: i32) -> i32 {
+    let mass = (mass / 3) - 2;
+    if mass <= 0 {
+        0
+    } else if mass < 6 {
+        mass
+    } else {
+        mass + get_recursive_fuel(mass)
+    }
+}
+
 fn main() {
 
     let data = get_data(1);
     let data = data.trim();
     let mut total = 0;
     for s in data.split("\n") {
-        println!("{}", s);
         match s.parse() {
-            Ok(n) => total += get_fuel(n),
+            Ok(n) => total += get_recursive_fuel(n),
             Err(_) => {
                 println!("Se encontro un error al parsear el valor {}", s);
                 continue
