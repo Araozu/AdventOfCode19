115790c4da52577280c642d68987bfbf6574ee91
Araozu <faraoz@unsa.edu.pe>
2021-03-01 11:59:51 -0500

    Add solution for day 3 part 2

--- src/dia03.rs
+++ src/dia03.rs
@@ -1,6 +1,6 @@
 use std::time::Instant;
 use crate::utils;
-use std::cmp::Ordering;
+use std::cmp::{Ordering, min};
 
 #[derive(Clone, Copy, Eq, PartialEq)]
 enum Orientation {
@@ -14,10 +14,18 @@ struct Point {
     y: i32
 }
 
+#[derive(Clone, Copy, Eq, PartialEq)]
+struct Point2 {
+    x: i32,
+    y: i32,
+    combined_steps: i32
+}
+
 struct Rect {
     start: Point,
     end: Point,
-    orientation: Orientation
+    orientation: Orientation,
+    steps: i32
 }
 
 impl Ord for Point {
@@ -34,7 +42,20 @@ impl PartialOrd for Point {
     }
 }
 
-fn parse_point(instruction: &str, previous_point: Point) -> (Point, Orientation) {
+impl Ord for Point2 {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.combined_steps.cmp(&other.combined_steps)
+    }
+}
+
+impl PartialOrd for Point2 {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+
+fn parse_point(instruction: &str, previous_point: Point) -> (Point, Orientation, i32) {
     let direction = &instruction[0..1].chars().nth(0).unwrap();
     let value = &instruction[1..].parse::<i32>().unwrap();
 
@@ -44,28 +65,28 @@ fn parse_point(instruction: &str, previous_point: Point) -> (Point, Orientation)
                 x: previous_point.x + value,
                 y: previous_point.y
             };
-            (next_point, Orientation::Horizontal)
+            (next_point, Orientation::Horizontal, value.clone())
         }
         'L' => {
             let next_point = Point {
                 x: previous_point.x - value,
                 y: previous_point.y
             };
-            (next_point, Orientation::Horizontal)
+            (next_point, Orientation::Horizontal, value.clone())
         }
         'U' => {
             let next_point = Point {
                 x: previous_point.x,
                 y: previous_point.y + value
             };
-            (next_point, Orientation::Vertical)
+            (next_point, Orientation::Vertical, value.clone())
         }
         'D' => {
             let next_point = Point {
                 x: previous_point.x,
                 y: previous_point.y - value
             };
-            (next_point, Orientation::Vertical)
+            (next_point, Orientation::Vertical, value.clone())
         }
         _ => {
             panic!("Invalid instruction in day 3")
@@ -75,16 +96,19 @@ fn parse_point(instruction: &str, previous_point: Point) -> (Point, Orientation)
 
 fn parse_map(data: &String, starting_point: Point) -> (Vec<Rect>, Vec<Rect>) {
     let mut previous_point = starting_point;
+    let mut steps = 0;
 
     let mut horizontal_rects = Vec::<Rect>::new();
     let mut vertical_rects = Vec::<Rect>::new();
 
     data.split(",").for_each(|x| {
-        let (next_point, orientation) = parse_point(x, previous_point);
+        let (next_point, orientation, new_steps) = parse_point(x, previous_point);
+        steps += new_steps;
         let return_value = Rect {
             start: previous_point,
             end: next_point,
-            orientation
+            orientation,
+            steps
         };
         previous_point = next_point;
 
@@ -101,8 +125,16 @@ fn parse_map(data: &String, starting_point: Point) -> (Vec<Rect>, Vec<Rect>) {
     (horizontal_rects, vertical_rects)
 }
 
+fn get_steps_horizontal(rect: &Rect, x: i32) -> i32{
+    rect.steps - (rect.end.x - x).abs()
+}
+
+fn get_steps_vertical(rect: &Rect, y: i32) -> i32 {
+    rect.steps - (rect.end.y - y).abs()
+}
+
 // Assumes one rect is vertical and the other is horizontal
-fn rect_intersection(rect_1: &Rect, rect_2: &Rect) -> Option<Point> {
+fn rect_intersection(rect_1: &Rect, rect_2: &Rect) -> Option<(Point, i32)> {
 
     let (vertical, horizontal) =
         match rect_1.orientation {
@@ -132,27 +164,61 @@ fn rect_intersection(rect_1: &Rect, rect_2: &Rect) -> Option<Point> {
     let rects_intersect = (horizontal_x_min <= vertical_x && vertical_x <= horizontal_x_max)
         && (vertical_y_min <= horizontal_y && horizontal_y <= vertical_y_max);
 
+    let point = Point {
+        x: vertical_x,
+        y: horizontal_y
+    };
+
     if rects_intersect {
-        Option::Some(Point {
-            x: vertical_x,
-            y: horizontal_y
-        })
+        Option::Some(
+            (
+                point,
+                get_steps_horizontal(horizontal, vertical_x)
+                    + get_steps_vertical(vertical, horizontal_y)
+            )
+        )
     } else {
         Option::None
     }
 }
 
 // Assumes one vector contains only vertical rects, and the other only horizontal rects
-fn find_crossing_points(first_path: Vec<Rect>, second_path: Vec<Rect>) -> Vec<Point> {
+fn find_crossing_points(first_path: &Vec<Rect>, second_path: &Vec<Rect>) -> Vec<Point> {
     let mut intersection_points = Vec::<Point>::new();
 
-    for r1 in &first_path {
-        for r2 in &second_path {
+    for r1 in first_path {
+        for r2 in second_path {
             match rect_intersection(r1, r2) {
-                Option::Some(point) => {
+                Option::Some((point, _)) => {
                     // Prevent the origin (0, 0) from counting
                     if !(point.x == 0 && point.y == 0) {
                         intersection_points.push(point)
+                    }
+                }
+                _ => {}
+            }
+        }
+    };
+
+    intersection_points
+}
+
+// Assumes one vector contains only vertical rects, and the other only horizontal rects
+fn find_crossing_points2(first_path: &Vec<Rect>, second_path: &Vec<Rect>) -> Vec<Point2> {
+    let mut intersection_points = Vec::<Point2>::new();
+
+    for r1 in first_path {
+        for r2 in second_path {
+            match rect_intersection(r1, r2) {
+                Option::Some((point, combined_steps)) => {
+                    // Prevent the origin (0, 0) from counting
+                    if !(point.x == 0 && point.y == 0) {
+
+                        intersection_points.push(Point2 {
+                            x: point.x,
+                            y: point.y,
+                            combined_steps
+                        })
                     }
                 }
                 _ => {}
@@ -176,28 +242,29 @@ pub fn run() {
     let (first_horizontal, first_vertical) = parse_map(first_path_str, p);
     let (second_horizontal, second_vertical) = parse_map(second_path_str, p);
 
-    // Find crossing points
-    let crossing_points_1 = find_crossing_points(first_horizontal, second_vertical);
-    let crossing_points_2 = find_crossing_points(first_vertical, second_horizontal);
+
+    // Find crossing points for part 1
+    let crossing_points_1 = find_crossing_points(&first_horizontal, &second_vertical);
+    let crossing_points_2 = find_crossing_points(&first_vertical, &second_horizontal);
 
     let min_1 = crossing_points_1.iter().min().unwrap();
     let min_2 = crossing_points_2.iter().min().unwrap();
 
-    let min = if min_1 < min_2 {
-        min_1
-    } else {
-        min_2
-    };
+    let min_part_1 = min(min_1, min_2);
+    let manhattan_distance_part_1 = min_part_1.x.abs() + min_part_1.y.abs();
 
-    println!("\tPart 3: {}", min.x.abs() + min.y.abs());
+
+    // Find crossing points for part 2
+    let crossing_points_1 = find_crossing_points2(&first_horizontal, &second_vertical);
+    let crossing_points_2 = find_crossing_points2(&first_vertical, &second_horizontal);
+
+    let min_1 = crossing_points_1.iter().min().unwrap();
+    let min_2 = crossing_points_2.iter().min().unwrap();
+
+    let min_part_2 = min(min_1, min_2);
+
+    println!("Day 2 results:");
+    println!("\tPart 1: {}", manhattan_distance_part_1);
+    println!("\tPart 2: {}", min_part_2.combined_steps);
     println!("\tMicroseconds: {:?}", time.elapsed().as_micros());
 }
-
-
-
-
-
-
-
-
-
